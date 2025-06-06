cf012ab3052a8fc9b0e70379102f7b6466db96f0
phrkdll <benjamin.hotop@gmail.com>
2025-06-06 08:45:34 +0200

    add interfaces and extend tests

--- pkg/must/error_action.go
+++ pkg/must/error_action.go
@@ -1,5 +1,19 @@
 package must
 
+type action interface {
+	Err() error
+}
+
+type responder interface {
+	action
+	Respond(responseWriter, int)
+}
+
+type panicer interface {
+	action
+	Panic()
+}
+
 type errorAction struct {
 	err error
 }
@@ -13,11 +27,17 @@ func newErrorAction(err error) errorAction {
 	return errorAction{err}
 }
 
-func (ea errorAction) Respond(w responseWriter, statusCode int) {
+func (ea errorAction) Err() error {
+	return ea.err
+}
+
+func (ea errorAction) Respond(w responseWriter, statusCode int) panicer {
 	if ea.err != nil {
 		w.WriteHeader(statusCode)
 		w.Write([]byte(ea.err.Error()))
 	}
+
+	return ea
 }
 
 func (ea errorAction) Panic() {
